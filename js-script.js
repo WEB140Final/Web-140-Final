895088b140825ee7002ac2a9faf01f8aefbb49c3
greenea628 <70304538+greenea628@users.noreply.github.com>
2020-12-09 16:00:21 -0500

    added jquery (#7)

--- js-script.js
+++ js-script.js
@@ -1,6 +1,33 @@
-window.alert("Javascript Example: Error 404");
-
 function darkMode() {
     var body = document.body;
         body.classList.toggle("dark-mode");
 }
+
+window.alert("Javascript Example: Error 404");
+
+$(document).ready (function(){
+
+
+$(".cosmetology-img").mouseenter (function() {
+    $(this).css("width", "85%");
+});
+$(".cosmetology-img").mouseleave (function() {
+    $(this).css("width", "80%");
+}
+);
+
+$(".brand").mouseenter (function() {
+    $(this).css("width", "105%");
+});
+$(".brand").mouseleave (function() {
+    $(this).css("width", "100%");
+}
+);
+
+$("#pageTitle").css ("color", "#02447e");
+
+
+
+});
+
+
